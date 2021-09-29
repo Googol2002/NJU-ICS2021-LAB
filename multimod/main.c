c438833bcb3bd636b5ec806c8b11b837b0cd9c6e
tracer-nju <tracer@nju.edu.cn>
2021-09-29 09:57:17 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 30008b9feb4d43b5af4a121017ead9b8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:57:17 up 34 min,  2 users,  load average: 0.02, 0.05, 0.05

--- multimod/main.c
+++ multimod/main.c
@@ -9,8 +9,16 @@ void test(uint64_t a, uint64_t b, uint64_t m) {
   printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
 }
 
+void test_add(uint64_t a, uint64_t b, uint64_t m) {
+  #define U64 "%" PRIu64
+  printf(U64 " * " U64 " add_mod " U64 " = " U64 "\n", a, b, m, add_mod(a, b, m));
+}
+
 int main() {
   test(123, 456, 789);
   test(123, 456, -1ULL);
   test(-2ULL, -2ULL, -1ULL); // should be 1
+
+  uint64_t a = UINT64_MAX / 3, b = UINT64_MAX / 2, m = 10071;
+  test_add(a, b, m);
 }
