2458c22a86d35382db3e9e360ea9e448a0ea589d
201300052-Shi Haoyu <xingyuncao2002@outlook.com>
2021-09-29 09:38:31 +0800

    添加了判断有效位的功能

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,23 @@
 #include <stdint.h>
 
+uint64_t valid_bits(uint64_t n){
+  uint64_t valid;
+
+  for (valid = 0; n; ++valid)
+    n = n >> 1;
+
+  return valid;
+}
+
+uint64_t mod(uint64_t x, uint64_t m){
+  uint64_t valid = valid_bits(m);
+
+  
+}
+
+
+
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   return (a * b) % m; // buggy
 }
