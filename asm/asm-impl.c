c0c1134058d115d821aa92d96c10462c68376a27
tracer-nju <tracer@nju.edu.cn>
2021-10-28 14:14:10 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 3b2b5f78b9b44a428d2729eb76217d23     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:14:10 up  1:12,  0 users,  load average: 0.09, 0.10, 0.03

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -14,9 +14,53 @@ int64_t asm_add(int64_t a, int64_t b) {
 
 int asm_popcnt(uint64_t x) {
   int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
+  //有效在%0中
+  __asm__(
+  "movq %2, %0;"\
+  "shrq %2, $1;"\
+  "andq %2, %3;"\
+  "andq %0, %3;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"\
+
+  "shrq %2, $2;"\
+  "andq %2, %4;"\
+  "andq %0, %4;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"\
+
+  "shrq %2, $4;"\
+  "andq %2, %5;"\
+  "andq %0, %5;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"\
+
+  "shrq %2, $8;"\
+  "andq %2, %6;"\
+  "andq %0, %6;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"\
+
+  "shrq %2, $16;"\
+  "andq %2, %7;"\
+  "andq %0, %7;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"\
+
+  "shrq %2, $32;"\
+  "andq %2, %8;"\
+  "andq %0, %8;"\
+  "addq %2, %0;"\
+  "movq %0, %2;"
+  : 
+  "=r" (s):
+  "0" (s), "r" (x), "n"(0x5555555555555555), "n"(0x3333333333333333), "n"(0x0F0F0F0F0F0F0F0F),
+   "n"(0x00FF00FF00FF00FF), "n"(0x0000FFFF0000FFFF), "n"(0x00000000FFFFFFFF)
+  );
+  
+  // for (int i = 0; i < 64; i++) {
+  //   if ((x >> i) & 1) s++;
+  // }
   return s;
 }
 
