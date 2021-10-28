ba60fb5e4c73f4654679b0aae5c2849ecad37949
tracer-nju <tracer@nju.edu.cn>
2021-10-28 14:47:26 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 3b2b5f78b9b44a428d2729eb76217d23     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:47:25 up  1:45,  1 user,  load average: 0.05, 0.04, 0.02

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -69,9 +69,9 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
     "mov $0, %%rsi;"\
     "1:;"\
     "mov 0(%0, %%rsi, 1), %%rax;"\
-    "mov %%rax, (%1, %%rsi)"\
-    "dec %%rsi"\
-    "cmp %%rsi, %2"\
+    "mov %%rax, (%1, %%rsi);"\
+    "dec %%rsi;"\
+    "cmp %%rsi, %2;"\
     "jl %1b":
     :
     "r"(src), "r"(dest), "r"(n):
