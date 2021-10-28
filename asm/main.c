adf627a369c4791ca0634955278a755b1fa7336d
tracer-nju <tracer@nju.edu.cn>
2021-10-28 15:02:24 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 3b2b5f78b9b44a428d2729eb76217d23     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:02:24 up  2:00,  1 user,  load average: 0.18, 0.07, 0.04

--- asm/main.c
+++ asm/main.c
@@ -1,4 +1,5 @@
 #include "asm.h"
+#include <string.h>
 #include <assert.h>
 #include <stdio.h>
 
@@ -8,7 +9,13 @@ int main() {
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-    // TODO: add more tests here.
+    
+    char src[32] = "12345678";
+    char dst[32];
+
+    asm_memcpy(dst, src, 9);
+    assert(strcmp(src, dst) == 0);
+
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
