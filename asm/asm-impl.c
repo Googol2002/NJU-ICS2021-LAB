d580f4794254b577c7bd786d63ca9e80bcad5e93
tracer-nju <tracer@nju.edu.cn>
2021-11-02 21:02:59 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:02:59 up  4:49,  3 users,  load average: 1.18, 1.21, 1.05

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -3,8 +3,12 @@
 int64_t asm_add(int64_t a, int64_t b) {
   int64_t result = 0;
   __asm__(
+  "movq (%%rbp), %%rax;"\
   "movq 16(%%rbp), %%rax;"\
+  "addq 24(%%rbp), %%rax;"\
+  "movq %%rax, -8(%%rbp)"
   );
+
   return result;
 }
 
