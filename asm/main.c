ec6bb2eb21305ca05e84e1d070d40753ad48755b
tracer-nju <tracer@nju.edu.cn>
2021-11-03 09:58:45 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:58:45 up  8:33,  3 users,  load average: 0.11, 0.07, 0.13

--- asm/main.c
+++ asm/main.c
@@ -5,21 +5,20 @@
 
 int main() {
   asm_jmp_buf buf;
-  int r = asm_setjmp(buf);
   int a = 1, b = 2, c = 3, d = 4, e = 5;
+  char src[32] = "12345678";
+  char dst[32];
+  int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     
-    char src[32] = "12345678";
-    char dst[32];
-
     asm_memcpy(dst, src, 9);
-    assert(strcmp(src, dst) == 0);
 
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
+    assert(strcmp(src, dst) == 0);
     assert(a == 1);
     assert(b == 2);
     assert(c == 3);
