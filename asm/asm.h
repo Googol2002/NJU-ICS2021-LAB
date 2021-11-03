6fbf34ba799ebb979e346c66619f334d6951761e
tracer-nju <tracer@nju.edu.cn>
2021-11-03 20:01:30 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: fd092a4fe21e4534bd63ffb26660eee7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:01:30 up  1:25,  1 user,  load average: 0.28, 0.26, 0.17

--- asm/asm.h
+++ asm/asm.h
@@ -12,4 +12,5 @@ int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
 void   *asm_memcpy(void *dest, const void *src, size_t n);
 int     asm_setjmp(asm_jmp_buf env) __attribute__ ((__nothrow__));
-void    asm_longjmp(asm_jmp_buf env, int val) __attribute__ ((__nothrow__))  __attribute__ ((__noreturn__));
+void    asm_longjmp(asm_jmp_buf env, int val) __attribute__ ((__nothrow__));
+//  __attribute__ ((__noreturn__))
