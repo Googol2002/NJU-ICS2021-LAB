e1aa6f7bafe8b64fe4894a151ff52a9b6c4a7111
tracer-nju <tracer@nju.edu.cn>
2021-11-03 09:38:41 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:38:41 up  8:13,  2 users,  load average: 0.32, 0.14, 0.36

--- asm/main.c
+++ asm/main.c
@@ -19,6 +19,14 @@ int main() {
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
+    assert(asm_add(1234, 5678) == 6912);
+    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    
+    char src[32] = "12345678";
+    char dst[32];
+
+    asm_memcpy(dst, src, 9);
+    assert(strcmp(src, dst) == 0);
     printf("PASSED.\n");
   }
 }
