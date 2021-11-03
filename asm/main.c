03ec5929e2709d57d98f3ad30c38d36abd19a82c
tracer-nju <tracer@nju.edu.cn>
2021-11-03 17:50:17 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: fd092a4fe21e4534bd63ffb26660eee7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:50:17 up 38 min,  1 user,  load average: 0.19, 0.20, 0.20

--- asm/main.c
+++ asm/main.c
@@ -34,58 +34,17 @@
 //   }
 // }
 
-// #include <stdio.h>
-// #include <setjmp.h>
-
-// 儲存程式跳躍時所需之資訊
-// asm_jmp_buf jmpbuffer;
-
-// int fun_a(int v) {
-//   int r = v * 2 - 1;
-//   if (r < 0) {
-//     // 跳躍至 main 函數
-//     asm_longjmp(jmpbuffer, 1);
-//   }
-//   return r;
-// }
-// int fun_b(int v) {
-//   int r = fun_a(v) + 6;
-//   if (r > 10) {
-//     // 跳躍至 main 函數
-//     asm_longjmp(jmpbuffer, 2);
-//   }
-//   return r;
-// }
-// int fun_c(int v) {
-//   int r = fun_b(v) * 5 - 21;
-//   return r;
-// }
-// int main() {
-//   // 設定跳躍目標位置
-//   int jmpVal = asm_setjmp(jmpbuffer);
-//   if ( jmpVal == 1 ) {
-//     printf("fun_a errorn");
-//   } else if ( jmpVal == 2 ) {
-//     printf("fun_b errorn");
-//   } else {  // jmpVal == 0
-//     int x = -5;
-//     int result = fun_c(x);
-//     printf("Result = %dn", result);
-//   }
-//   return 0;
-// }
-
 #include <stdio.h>
 #include <setjmp.h>
 
-// 儲存程式跳躍時所需之資訊
-jmp_buf jmpbuffer;
+儲存程式跳躍時所需之資訊
+asm_jmp_buf jmpbuffer;
 
 int fun_a(int v) {
   int r = v * 2 - 1;
   if (r < 0) {
     // 跳躍至 main 函數
-    longjmp(jmpbuffer, 1);
+    asm_longjmp(jmpbuffer, 1);
   }
   return r;
 }
@@ -93,7 +52,7 @@ int fun_b(int v) {
   int r = fun_a(v) + 6;
   if (r > 10) {
     // 跳躍至 main 函數
-    longjmp(jmpbuffer, 2);
+    asm_longjmp(jmpbuffer, 2);
   }
   return r;
 }
@@ -103,7 +62,7 @@ int fun_c(int v) {
 }
 int main() {
   // 設定跳躍目標位置
-  int jmpVal = setjmp(jmpbuffer);
+  int jmpVal = asm_setjmp(jmpbuffer);
   if ( jmpVal == 1 ) {
     printf("fun_a errorn");
   } else if ( jmpVal == 2 ) {
@@ -115,3 +74,44 @@ int main() {
   }
   return 0;
 }
+
+// #include <stdio.h>
+// #include <setjmp.h>
+
+// // 儲存程式跳躍時所需之資訊
+// jmp_buf jmpbuffer;
+
+// int fun_a(int v) {
+//   int r = v * 2 - 1;
+//   if (r < 0) {
+//     // 跳躍至 main 函數
+//     longjmp(jmpbuffer, 1);
+//   }
+//   return r;
+// }
+// int fun_b(int v) {
+//   int r = fun_a(v) + 6;
+//   if (r > 10) {
+//     // 跳躍至 main 函數
+//     longjmp(jmpbuffer, 2);
+//   }
+//   return r;
+// }
+// int fun_c(int v) {
+//   int r = fun_b(v) * 5 - 21;
+//   return r;
+// }
+// int main() {
+//   // 設定跳躍目標位置
+//   int jmpVal = setjmp(jmpbuffer);
+//   if ( jmpVal == 1 ) {
+//     printf("fun_a errorn");
+//   } else if ( jmpVal == 2 ) {
+//     printf("fun_b errorn");
+//   } else {  // jmpVal == 0
+//     int x = -5;
+//     int result = fun_c(x);
+//     printf("Result = %dn", result);
+//   }
+//   return 0;
+// }
