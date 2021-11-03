0ff566f42c8a210fb647626dedf52458780cb13a
tracer-nju <tracer@nju.edu.cn>
2021-11-03 11:13:04 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:13:04 up  9:47,  2 users,  load average: 0.05, 0.08, 0.03

--- asm/main.c
+++ asm/main.c
@@ -4,18 +4,20 @@
 #include <stdio.h>
 
 int main() {
-  asm_jmp_buf buf;
-  int a = 1, b = 2, c = 3, d = 4, e = 5;
+  asm_jmp_buf buf1, buf2;
+  int a = 1, b = 2, c = 3, d = 4, e = 5, i = 9;
   char src[32] = "12345678";
   char dst[32];
-  int r = asm_setjmp(buf);
+  int r = asm_setjmp(buf1);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     
     asm_memcpy(dst, src, 9);
-
-    asm_longjmp(buf, 123);
+    if (asm_setjmp(buf2)){
+      
+    }
+    asm_longjmp(buf1, 123);
   } else {
     assert(r == 123);
     assert(strcmp(src, dst) == 0);
@@ -24,7 +26,10 @@ int main() {
     assert(c == 3);
     assert(d == 4);
     assert(e == 5);
-
+    i--;
+    if (i > 0){
+      asm_longjmp(buf2, 1);
+    }
     printf("PASSED.\n");
   }
 }
