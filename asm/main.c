19633defac0a439740f81a6088f2f7c0811f896a
tracer-nju <tracer@nju.edu.cn>
2021-11-04 16:56:25 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 15e9808196024ddb9c4ab63b2001c539     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  16:56:26 up  3:07,  2 users,  load average: 0.42, 0.27, 0.16

--- asm/main.c
+++ asm/main.c
@@ -3,66 +3,59 @@
 #include <assert.h>
 #include <stdio.h>
 
-int main() {
-  asm_jmp_buf buf1, buf2;
-  int a = 1, b = 2, c = 3, d = 4, e = 5, i = 9;
-  int f = 1, g = 2, h = 3, j = 4, k = 5, l = 9;
-  char src[32] = "12345678";
-  char dst[32];
-  char arr1[] = "abcdefghijk";
-  int r = asm_setjmp(buf1);
-  char arr2[] = "abcdefghijk";
-  if (r == 0) {
-    assert(asm_add(1234, 5678) == 6912);
-    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+// int main() {
+//   asm_jmp_buf buf1, buf2;
+//   int a = 1, b = 2, c = 3, d = 4, e = 5, i = 9;
+//   int f = 1, g = 2, h = 3, j = 4, k = 5, l = 9;
+//   char src[32] = "12345678";
+//   char dst[32];
+//   char arr1[] = "abcdefghijk";
+//   int r = asm_setjmp(buf1);
+//   char arr2[] = "abcdefghijk";
+//   if (r == 0) {
+//     assert(asm_add(1234, 5678) == 6912);
+//     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     
-    asm_memcpy(dst, src, 9);
-    if (asm_setjmp(buf2)){
+//     asm_memcpy(dst, src, 9);
+//     if (asm_setjmp(buf2)){
       
-    }
-    asm_longjmp(buf1, 123);
-  } else {
-    assert(r == 123);
-    assert(strcmp(src, dst) == 0);
-    assert(a == 1);
-    assert(b == 2);
-    assert(c == 3);
-    assert(d == 4);
-    assert(e == 5);
-    i--;
-    if (i > 0){
-      asm_longjmp(buf2, 1);
-    }
-    assert(strcmp(arr1, arr2) == 0);
+//     }
+//     asm_longjmp(buf1, 123);
+//   } else {
+//     assert(r == 123);
+//     assert(strcmp(src, dst) == 0);
+//     assert(a == 1);
+//     assert(b == 2);
+//     assert(c == 3);
+//     assert(d == 4);
+//     assert(e == 5);
+//     i--;
+//     if (i > 0){
+//       asm_longjmp(buf2, 1);
+//     }
+//     assert(strcmp(arr1, arr2) == 0);
 
-    printf("PASSED.\n");
-  }
+//     printf("PASSED.\n");
+//   }
+// }
+
+static asm_jmp_buf buf;
+
+void second() {
+    printf("second\n");         // prints
+    asm_longjmp(buf,1);             // jumps back to where setjmp was called - making setjmp now return 1
 }
 
-// asm_jmp_buf my_jum_buf;
-// void fun_fun()
-// {
-//     printf("Enter fun_fun ...\n");
-//     asm_longjmp(my_jum_buf, 8);//如下代码不会被执行
-//     printf("fun_fun::can't see");
-// }
+void first() {
+    second();
+    printf("first\n");          // does not print
+}
 
-// void fun()
-// {
-//     fun_fun();
-// }
+int main() {   
+    if (!asm_setjmp(buf))
+        first();                // when executed, setjmp returned 0
+    else                        // when longjmp jumps back, setjmp returns 1
+        printf("main\n");       // prints
 
-// int main()
-// {
-//     int ret;
-//     if((ret = asm_setjmp(my_jum_buf), ret))
-//     {
-//         printf("Main: return after calling longjmp, ret = %d.\n", ret);
-//     }
-//     else
-//     {
-//         printf("Main: first time return from setjmp, ret = %d\n", ret);
-//         fun();
-//     }
-//     return 0;
-// }
+    return 0;
+}
