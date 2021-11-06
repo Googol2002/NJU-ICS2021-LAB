c81a5aacbe6faf0e868fe6fc74c8a2cae88600ef
tracer-nju <tracer@nju.edu.cn>
2021-11-06 13:24:03 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a8186582d8754f02afec51abfe9a1c44     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  13:24:03 up  3:35,  3 users,  load average: 0.22, 0.08, 0.10

--- asm/asm.h
+++ asm/asm.h
@@ -2,10 +2,11 @@
 #include <stddef.h>
 
 // typedef uint64_t __my_jmp_buf[8];
-struct __my__jmp_buf_struct{
-    uintptr_t __jmpbuf[8];
-};
-typedef struct __my__jmp_buf_struct my_jmp_buf[1];
+// struct __my__jmp_buf_struct{
+//     uintptr_t __jmpbuf[8];
+// };
+// typedef struct __my__jmp_buf_struct my_jmp_buf[1];
+typedef uint64_t my_jmp_buf[8];
 #define asm_jmp_buf my_jmp_buf
 
 int64_t asm_add(int64_t a, int64_t b);
