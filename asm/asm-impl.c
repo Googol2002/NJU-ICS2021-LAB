4b1c562a007d6f8117a2928f9e6281d246ceb7c6
tracer-nju <tracer@nju.edu.cn>
2021-11-06 13:15:46 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a8186582d8754f02afec51abfe9a1c44     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  13:15:46 up  3:26,  3 users,  load average: 0.13, 0.10, 0.14

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -77,7 +77,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
   return dest;
 }
 
-int asm_setjmp(asm_jmp_buf env) {
+int volatile asm_setjmp(asm_jmp_buf env) {
    __asm__(
     // 存rbx
     "movq %%rbx, 8(%%rdi);"\
@@ -101,7 +101,7 @@ int asm_setjmp(asm_jmp_buf env) {
   return 0;
 }
 
-void asm_longjmp(asm_jmp_buf env, int val) {
+void volatile asm_longjmp(asm_jmp_buf env, int val) {
   __asm__(
     "movl %%esi, %%eax;"\
     // 恢复saved rbp
