9d528fe4ca6eba5d07d072312cd3006457d66ddb
tracer-nju <tracer@nju.edu.cn>
2021-11-22 21:48:44 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d35df05674ea4645aeefb0362b06a8d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:48:44 up  1:10,  2 users,  load average: 0.00, 0.00, 0.00

--- perftune/main.c
+++ perftune/main.c
@@ -4,9 +4,10 @@
 int *sieve(int n);
 
 int main() {
-  int *primes = sieve(400);
+  int *primes = sieve(100000);
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
   }
+  
 }
--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,26 +2,37 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define N 10000000
 
-static bool is_prime[N];
-static int  primes[N];
+static int is_prime[N] = {0};
+static int primes[N];
+
+#define ABS(x) (x > 0 ? x : -x)
+
+static uint16_t my_sqrt(uint32_t n){
+  double guess = n;
+  while (ABS(guess * guess - n) >= 0.25){
+    guess = (guess + n / guess) / 2.0;
+  }
+
+  return (uint16_t)guess + 1;
+}
+
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
 
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i <= (n); i++) {//O(n^2)变为O(n^{3/2})
     for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+      is_prime[j] = 1; // 用1表示false，0默认为true
     }
   }
 
   int *p = primes;
-  for (int i = 2; i <= n; i++)
-    if (is_prime[i]) {
+  for (int i = 2; i <= n; i++)//O(n)
+    if (!is_prime[i]) {// 0默认为true
       *p++ = i;
     }
   *p = 0;
