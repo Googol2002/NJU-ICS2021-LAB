9c492b7747c4cf7eb4e8f370a17e4a1c672ca21c
tracer-nju <tracer@nju.edu.cn>
2021-11-23 07:05:31 -0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a8f25aad9f1d47a6bac9f95eaa3f2813     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  07:05:31 up  1:24,  2 users,  load average: 0.03, 0.13, 0.07

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,7 +9,7 @@
 #define TRUE 0
 #define FALSE 1
 
-static bool is_prime[N] = {0};
+static bool is_prime[N] = {};
 static int primes[N];
 
 #define MIN(x, y) (x > y ? y : x)
@@ -28,22 +28,25 @@ int *sieve(int n) {
   assert(n + 1 < N);
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
+  is_prime[2] = false;
 
-  for (int i = 2; i <= n; i++) {
+  for (int i = 3; i <= n; i += 2) {
     if(is_prime[i]){
-      int product = i * i;
+      int product = i * i, step = 2 * i;
       if (product > n){
         break;
       }
-      for (int j = product; j <= n; j += i) {
+      // 对于 i * k (k < i) 应该在 i开始前被筛掉，因为i * k 有因子 k 小于 i
+      // i * (i + 2k * i) i必为奇数 i + 2k * i 必为奇数
+      for (int j = product; j <= n; j += step) {
         is_prime[j] = false;
       }
     }
-      
   }
 
   int *p = primes;
-  for (int i = 2; i <= n; i++)
+  *p++ = 2;
+  for (int i = 2; i <= n; i += 2)
     if (is_prime[i]) {
       *p++ = i;
     }
